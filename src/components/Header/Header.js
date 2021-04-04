9b96a422db31fe6ac3dbdff91f50ed8e0f1f5773
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-04-04 20:45:11 +0300

    work logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,25 @@ import UserMenu from 'components/UserMenu';
 import { useSelector } from 'react-redux';
 import { authSelectors } from 'redux/auth';
 
-import styles from './Header.module.css'
+import styles from './Header.module.css';
 import Navigation from 'components/Navigation';
 
 export default function Header() {
   const isAuth = useSelector(authSelectors.getIsAuthUser);
 
   return (
-    <div className='header-wrapper'>
-      <h3>Logo</h3>
+    <div className={styles.headerWrapper}>
+      <div className={styles.logo}>
+        <div className={styles.pro}>
+          <b className={styles.textpro}>Pro</b>
+        </div>
+        <div className={styles.test}>
+          <b className={styles.texttest}>[Test_]</b>
+        </div>
+      </div>
       {/* {isAuth && <UserMenu />} */}
       <Navigation />
-      <UserMenu />
+      {/* <UserMenu /> */}
     </div>
-  )
+  );
 }
