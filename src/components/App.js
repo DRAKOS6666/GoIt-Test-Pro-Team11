df460950ba26b8f22f3b6b1df74de589e785f27f
Vetokh-Marina <marina.vetoh@gmail.com>
2021-04-07 00:17:11 +0300

    add style to section

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,93 @@
-import React, { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Redirect, Switch } from 'react-router-dom';
-import { authOperations } from 'redux/auth';
+// import React, { Suspense, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Redirect, Switch } from 'react-router-dom';
 
-import PrivateRoute from 'components/Route/PrivateRoute';
-import PublicRoute from 'components/Route/PublicRoute';
+// import { authOperations } from 'redux/auth';
 
-import Home from 'components/Home';
-import Header from 'components/Header';
-import Navigation from 'components/Navigation';
-import Login from 'components/Login';
-import Test from 'components/Test';
-import Results from './Results';
+// import PrivateRoute from 'components/Route/PrivateRoute';
+// import PublicRoute from 'components/Route/PublicRoute';
+
+// import Home from 'components/Home';
+// import Header from 'components/Header';
+// import Navigation from 'components/Navigation';
+// import Login from 'components/Login';
+// import Test from 'components/Test';
+// import Results from './Results';
+import React from 'react';
+// import s from './App.css';
+
+
 import Materials from './Materials';
-import Contacts from 'views/Contacts';
-import Footer from 'views/Footer';
-import Loader from 'components/Loader';
+import { books, resources } from './Materials/usefullMaterials.json';
+// import Contacts from 'views/Contacts';
+// import Footer from 'views/Footer';
+// import Loader from 'components/Loader';
 
 
 
 import './index.css';
-
-
 function App() {
-  const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(authOperations.getCurrentUser());
-  // }, [dispatch]);
 
   return (
-    <div className="wrapper">
-      <Header>
-        <Navigation />
-      </Header>
-      <Suspense fallback={<Loader />}>
-        <Switch>
-
-          <PublicRoute
-            path="/auth"
-            component={Login}
-            restricted
-          />
-          <PublicRoute
-            path="/test"
-            component={Test}
-            redirectTo="/auth"
-          />
-          <PublicRoute
-            path="/results"
-            component={Results}
-            redirectTo="/auth"
-          />
-          <PublicRoute
-            path="/useful-info"
-            component={Materials}
-            redirectTo="/auth"
-          />
-          <PublicRoute
-            path="/contacts"
-            component={Contacts}
-            redirectTo="/auth"
-          />
-          <PublicRoute
-            path="/"
-            component={Home}
-            redirectTo="/auth"
-          />
-
-          <Redirect to="/auth" />
-
-        </Switch>
-        <Footer />
-      </Suspense>
+    <div>
+      <Materials books={books} resources={resources} />
     </div>
   );
 }
+
+// function App() {
+//   const dispatch = useDispatch();
+
+//   // useEffect(() => {
+//   //   dispatch(authOperations.getCurrentUser());
+//   // }, [dispatch]);
+
+//   return (
+//     <div className="wrapper">
+//       <Header>
+//         <Navigation />
+//       </Header>
+//       <Suspense fallback={<Loader />}>
+//         <Switch>
+
+//           <PublicRoute
+//             path="/auth"
+//             component={Login}
+//             restricted
+//           />
+//           <PublicRoute
+//             path="/test"
+//             component={Test}
+//             redirectTo="/auth"
+//           />
+//           <PublicRoute
+//             path="/results"
+//             component={Results}
+//             redirectTo="/auth"
+//           />
+//           <PublicRoute
+//             path="/useful-info"
+//             component={Materials}
+//             redirectTo="/auth"
+//           />
+//           <PublicRoute
+//             path="/contacts"
+//             component={Contacts}
+//             redirectTo="/auth"
+//           />
+//           <PublicRoute
+//             path="/"
+//             component={Home}
+//             redirectTo="/auth"
+//           />
+
+//           <Redirect to="/auth" />
+
+//         </Switch>
+//         <Footer />
+//       </Suspense>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Materials/Materials.js
+++ src/components/Materials/Materials.js
@@ -1,24 +1,24 @@
 import React from 'react';
 // import { Link, NavLink } from 'react-router-dom';
 import UsefullInfoItem from './UsefullInfoItem/UsefullInfoItem'
-import styles from './Materials.module.css';
+import s from './Materials.module.css';
 
 function UsefullInfo({ books, resources }) {
     return (
-        <>
-            <h2>Usefull literature</h2>
-            <ol className={styles.s}>
+        <div className={s.section}>
+            <h2 className={s.title}>Usefull literature</h2>
+            <ol className={s.list}>
                 {books.map(({ id, name, url }) => (
-                    <li className={styles.s} key={id}><UsefullInfoItem id={id} name={name} url={url} /></li>
+                    <li className={s.materials} key={id}><UsefullInfoItem id={id} name={name} url={url} /></li>
                 ))}
             </ol>
-            <h2>Usefull resources</h2>
-            <ol className={styles.s}>
+            <h2 className={s.title}>Usefull resources</h2>
+            <ol className={s.list}>
                 {resources.map(({ id, name, url }) => (
-                    <li className={styles.s} key={id}><UsefullInfoItem id={id} name={name} url={url} /></li>
+                    <li className={s.materials} key={id}><UsefullInfoItem id={id} name={name} url={url} /></li>
                 ))}
             </ol>
-        </>
+        </div>
     );
 }
 export default UsefullInfo
