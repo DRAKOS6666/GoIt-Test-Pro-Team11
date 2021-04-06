e3f8afdb6db1369ce63b57901b455fd818cf8394
Yulia <sivonenkoyulia@gmail.com>
2021-04-06 23:28:18 +0300

    init footer

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import styles from './Footer.module.css';
+import s from './Footer.module.css';
 
 const Footer = () => {
 
     return (
-        <h3>
-            Footer
-        </h3>
+        <footer className={s.footer}>
+            <div className={s.footer_wrap}>
+                <p className={s.footer_descr}> <span className={s.footer_descr_icon}>©</span>  2021</p>
+                <p className={s.footer_descr}> <span>|</span> All Rights Reserved</p>
+                <p className={s.footer_descr}> <span>|</span> Developed with</p>
+
+            </div>
+            <p className={s.footer_des}> <a href="#" className={s.footer_descr_link}>by GoIT Students</a></p>
+
+        </footer>
     );
 };
 export default Footer;
