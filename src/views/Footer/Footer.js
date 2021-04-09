e9072304e348d46d9da1dbad4ed8e0be7dc04b85
Yulia <sivonenkoyulia@gmail.com>
2021-04-10 01:18:23 +0300

    init footer

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
-
 import s from './Footer.module.css';
+import heart from '../../images/icons/heart.svg'
 
 const Footer = () => {
 
     return (
-        <footer className={s.footer}>
+        <footer className={s.wrapper} >
             <div className={s.footer_wrap}>
-                <p className={s.footer_descr}> <span className={s.footer_descr_icon}>©</span>  2021</p>
-                <p className={s.footer_descr}> <span>|</span> All Rights Reserved</p>
-                <p className={s.footer_descr}> <span>|</span> Developed with</p>
-
+                <p className={s.footer_descr}> <span className={s.footer_descr_icon}>©</span> 2021</p>
+                <p className={s.footer_descr}> <span className={s.footer_descr_span}>|</span> All Rights Reserved</p>
+                <p className={s.footer_descr}> <span className={s.footer_descr_span}>|</span> Developed with</p>
+                <img className={s.footer_icon} src={heart} alt="" />
+                <p className={s.footer_descr_link}>by GoIT Students</p>
             </div>
-            <p className={s.footer_des}> <a href="#" className={s.footer_descr_link}>by GoIT Students</a></p>
+
 
         </footer>
     );
