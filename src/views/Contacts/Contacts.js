1f66be06ba207c5090cf62aab5b3a8311ed1028c
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-04-13 23:39:47 +0300

    add json

--- src/views/Contacts/Contacts.js
+++ src/views/Contacts/Contacts.js
@@ -3,9 +3,6 @@ import React from 'react';
 import styles from './Contacts.module.css';
 
 const Contacts = () => {
-
-    return (
-        <h2>Contacts</h2>
-    );
+  return <h2>Contacts</h2>;
 };
 export default Contacts;
