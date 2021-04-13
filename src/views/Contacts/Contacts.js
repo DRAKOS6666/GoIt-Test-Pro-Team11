7b5edfd1ebb67a426a6a48ba751644096d98b550
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-04-14 01:16:06 +0300

    add ul with contacts

--- src/views/Contacts/Contacts.js
+++ src/views/Contacts/Contacts.js
@@ -1,8 +1,24 @@
 import React from 'react';
-
+import team from '../team.json';
 import styles from './Contacts.module.css';
 
-const Contacts = () => {
-  return <h2>Contacts</h2>;
-};
-export default Contacts;
+export default function Contacts() {
+  console.log(team);
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>Our team</h2>
+      <ul className={styles.list}>
+        {team.map((el, index) => (
+          <li key={index} className={styles.listItem}>
+            <img src={el.url} className={styles.photo} />
+            <div className={styles.content}>
+              <p className={styles.name}>{el.name}</p>
+              <p className={styles.vocation}>{el.vocation}</p>
+              <p className={styles.description}>{el.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
