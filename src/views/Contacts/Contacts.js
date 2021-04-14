b0ca37c9fafec831c5b24c48065cd1bf7a76ce26
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-04-14 10:46:38 +0300

    add description and alt

--- src/views/Contacts/Contacts.js
+++ src/views/Contacts/Contacts.js
@@ -3,14 +3,13 @@ import team from '../team.json';
 import styles from './Contacts.module.css';
 
 export default function Contacts() {
-  console.log(team);
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Our team</h2>
       <ul className={styles.list}>
         {team.map((el, index) => (
           <li key={index} className={styles.listItem}>
-            <img src={el.url} className={styles.photo} />
+            <img src={el.url} alt={el.alt} className={styles.photo} />
             <div className={styles.content}>
               <p className={styles.name}>{el.name}</p>
               <p className={styles.vocation}>{el.vocation}</p>
