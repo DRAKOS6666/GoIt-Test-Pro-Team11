fd401f5bdb216133b3a8265bf38196bdf9683f8f
DRAKOS <drakostius2@gmail.com>
2021-04-15 23:27:15 +0300

    Help with redux middleware

--- src/redux/middlewares/refreshToken.js
+++ src/redux/middlewares/refreshToken.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { authOperations } from 'redux/auth';
 import { refreshToken } from '../../services/auth-api';
 
 const token = {
@@ -38,64 +36,5 @@ const refreshTokenMiddleware = ({ getState, dispatch }) => next => action => {
   handleExpiredToken();
 
   return next(action);
-  // const theStore = store.getState()
-  // console.log('theStore', theStore)
-  // console.log('action', action.requestStatus)
-  // try {
-  //     const {
-  //         response,
-  //     } = action;
-  //     // console.log('response', response)
-  //     if (!response) {
-  //         return next(action);
-  //     }
-
-  //     // console.log('itsGood')
-  //     // const { sid, refreshToken } = store.getState()
-  //     // console.log('sid and rToken', sid, refreshToken)
-  //     // const response = authOperations.refreshUser(refreshToken, sid)
-  //     // return response
-  //     return next(action)
-  // } catch (err) {
-  //     console.log('errorHandle', err)
-  //     const { sid, refreshToken } = store.getState()
-
-  //     const response = authOperations.refreshUser(refreshToken, sid)
-  //     return response
-  //   Raven.captureException(err, {
-  //     extra: {
-  //       action,
-  //       state: store.getState()
-  //     }
-  //   })
-  //   throw err
-  // }
 };
 export default refreshTokenMiddleware;
-// export default function refreshToken() {
-//     return ({ dispatch, getState }) => next => (action) => {
-//         console.log('action', action)
-//         const {
-//             request,
-//         } = action;
-
-//         if (!request) {
-//             return next(action);
-//         }
-
-//         const { sid, refreshToken } = getState().auth;
-
-
-//         if (refreshToken && sid) {
-//             const response = authOperations.refreshUser(sid, refreshToken)
-//             dispatch({ accesToken: response.newAccessToken, refreshToken: response.newRefreshToken, sid: response.newSid })
-//             // axios.post('/path/to/renew')
-//             //     .then({ refresh_token: tokens.refresh_token })
-//             //     .catch((err, { body } = {}) => {
-//             //         dispatch({ type: 'SET_TOKENS', payload: body });
-//             //         request(body);
-//             //     });
-//         }
-//         return next(action);
-//     };
-// }
