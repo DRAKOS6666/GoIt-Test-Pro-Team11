e451830e36e47e429581f71f2622ec24cebc02e0
Dmitriy Khomiakov <unstopddd@gmail.com>
2021-04-15 20:46:24 +0300

    Update Results.js

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,51 +5,32 @@ import { testOperations, testSelectors } from 'redux/qaTest';
 import Diagram from '../Diagram';
 import s from './Results.module.css';
 import resultsImg from '../../images/results.png';
-
 export default function Results() {
   const resultInfo = useSelector(testSelectors.getTestResults);
   const history = useHistory();
   const location = useLocation();
   console.log('history', history);
   console.log('location', location);
-
   const backToTestPage = () => {
     history.push('/test');
   };
   const mainMessage = resultInfo.mainMessage;
   const secondaryMessage = resultInfo.secondaryMessage;
   const resultInPercents = resultInfo.result;
-
   /*  const testName =  */
-
   /*    const dispatch = useDispatch(); */
   const resultNumber = Number(
     resultInPercents ? resultInPercents.slice(0, -1) : 0,
   );
   const incorrectNumber = 100 - resultNumber;
-
   const totalQuestions = 12;
   const correctAnswers = parseInt((totalQuestions * resultNumber) / 100, 10);
-
   return (
     <div className={s.container}>
       <h2>Results</h2>
-
-  //   const totalQuestions = 12;
-  //   const resultNumber = useMemo(() => {return Number(
-  //   resultInPercents ? resultInPercents.slice(0, -1) : 0,
-  //   )}, []);
-  //  const incorrectNumber = useMemo(() => {return 100 - resultNumber}, []);
-  //   const correctAnswers =useMemo(() => {return parseInt((totalQuestions * resultNumber) / 100, 10)}, []);
-  //   /*    const dispatch = useDispatch(); */
-  return (
-    <div className={s.container}>
-      <h2 className={s.title}>Results</h2>
-      <h3 className={s.subtitle}>[TESTING THEORY_]</h3>
-      {/* <h3 className={s.subtitle}>[{testName}_]</h3> */}
-
+      <h3>[TESTING THEORY_]</h3>
+      {/* <h3>[{testName}_]</h3> */}
       <div className={s.line}></div>
-
       <div>
         <Diagram
           data={[
@@ -59,7 +40,6 @@ export default function Results() {
           ]}
         />
       </div>
-
       <p>Correct answers - {correctAnswers} </p>
       <p>Total questions - {totalQuestions} </p>
       <div className={s.back}>
@@ -70,11 +50,10 @@ export default function Results() {
       </div>
       <p>{mainMessage}</p>
       <p>{secondaryMessage}</p>
-
       <button className={s.button} type="button" onClick={backToTestPage}>
+        {' '}
         Try again
       </button>
-
       {/*   {{testName} === 'Testing Theory' ? <button
 	@@ -68,6 +76,6 @@ export default function Results () {
         type="button"
