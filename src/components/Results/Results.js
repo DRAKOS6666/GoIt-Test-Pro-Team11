59d450558e08f157ac40d609ef1de0066d52d762
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2021-04-15 21:50:23 +0300

    v3

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory, useLocation } from 'react-router-dom';
-import { testOperations, testSelectors } from 'redux/qaTest';
-import Diagram from '../Diagram';
-import s from './Results.module.css';
-import resultsImg from '../../images/results.png';
+// import React, { useEffect, useMemo } from 'react';
+// import { useSelector } from 'react-redux';
+// import { useHistory, useLocation } from 'react-router-dom';
+// import { testOperations, testSelectors } from 'redux/qaTest';
+// import Diagram from '../Diagram';
+// import s from './Results.module.css';
+// import resultsImg from '../../images/results.png';
 
-export default function Results() {
-  const resultInfo = useSelector(testSelectors.getTestResults);
-  const history = useHistory();
-  const location = useLocation();
-  console.log('history', history);
-  console.log('location', location);
+// export default function Results() {
+//   const resultInfo = useSelector(testSelectors.getTestResults);
+//   const history = useHistory();
+//   const location = useLocation();
+//   console.log('history', history);
+//   console.log('location', location);
 
-  const backToTestPage = () => {
-    history.push('/test');
-  };
-  const mainMessage = resultInfo.mainMessage;
-  const secondaryMessage = resultInfo.secondaryMessage;
-  const resultInPercents = resultInfo.result;
+//   const backToTestPage = () => {
+//     history.push('/test');
+//   };
+//   const mainMessage = resultInfo.mainMessage;
+//   const secondaryMessage = resultInfo.secondaryMessage;
+//   const resultInPercents = resultInfo.result;
 
-  /*  const testName =  */
+//   /*  const testName =  */
 
-  /*    const dispatch = useDispatch(); */
-  const resultNumber = Number(
-    resultInPercents ? resultInPercents.slice(0, -1) : 0,
-  );
-  const incorrectNumber = 100 - resultNumber;
+//   /*    const dispatch = useDispatch(); */
+//   const resultNumber = Number(
+//     resultInPercents ? resultInPercents.slice(0, -1) : 0,
+//   );
+//   const incorrectNumber = 100 - resultNumber;
 
-  const totalQuestions = 12;
-  const correctAnswers = parseInt((totalQuestions * resultNumber) / 100, 10);
+//   const totalQuestions = 12;
+//   const correctAnswers = parseInt((totalQuestions * resultNumber) / 100, 10);
